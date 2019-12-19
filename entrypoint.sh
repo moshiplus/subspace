29c21f864ec9d25f219e7fdb753d94f043d32a48
moshiplus <31259625+moshiplus@users.noreply.github.com>
2019-12-20 01:55:22 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ -z "${SUBSPACE_BACKLINK-}" ] ; then
 fi
 
 if [ -z "${SUBSPACE_LETSENCRYPT-}" ] ; then
-    export SUBSPACE_LETSENCRYPT="true"
+    export SUBSPACE_LETSENCRYPT="false"
 fi
 
 if [ -z "${SUBSPACE_HTTP_ADDR-}" ] ; then
